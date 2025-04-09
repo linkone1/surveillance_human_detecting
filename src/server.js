7b02d2504264277464b9373f524ab1a409aa307e
Linkan333 <linunybe@gmail.com>
2025-04-09 21:25:23 +0200

    Security feature removed the smtp server

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.static(rootDir));
 
 // SMTP Configuration
 const transporter = nodemailer.createTransport({
-    host: 'send.one.com',
+    host: '[REDACTED]',
     port: 465,
     secure: true,
     auth: {
